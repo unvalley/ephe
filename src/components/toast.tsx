576a2dde092611eab31918842557d04976c1bc88
unvalley <kirohi.code@gmail.com>
2025-03-26 21:27:37 +0900

    chore: reduce lint errors

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -13,7 +13,7 @@ export const Toast = ({ message, type = "success", duration = 3000, onClose }: T
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(false);
-      setTimeout(onClose, 300); // 300msのフェードアウト後に完全に削除
+      setTimeout(onClose, 300);
     }, duration);
 
     return () => clearTimeout(timer);
@@ -100,8 +100,10 @@ export const Toast = ({ message, type = "success", duration = 3000, onClose }: T
             setTimeout(onClose, 300);
           }}
           className="ml-4 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+          type="button"
         >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+            <title>Close icon</title>
             <path
               fillRule="evenodd"
               d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
