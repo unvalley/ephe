1cc71ab6f66d35dc8ae03952eb87b5634e54ea10
unvalley <kirohi.code@gmail.com>
2025-04-02 02:00:20 +0900

    refactor: dexie

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,3 @@
-// Local storage keys
 export const EDITOR_CONTENT_KEY = "editor-content";
 export const COMPLETED_TASKS_KEY = "ephe-completed-tasks";
 export const SNAPSHOTS_KEY = "ephe-snapshots"; 
