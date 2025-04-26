dd2989471e4e989af26056fadb90f163a061c002
unvalley <kirohi.code@gmail.com>
2025-04-26 15:31:06 +0900

    chore: mode

--- src/features/editor/tasks/task-auto-flush.ts
+++ src/features/editor/tasks/task-auto-flush.ts
@@ -4,6 +4,6 @@ import { LOCAL_STORAGE_KEYS } from "../../../utils/constants";
 export type TaskAutoFlushMode = "off" | "instant";
 
 export const taskAutoFlushAtom = atomWithStorage<TaskAutoFlushMode>(
-  LOCAL_STORAGE_KEYS.TASK_AUTO_FLUSH,
+  LOCAL_STORAGE_KEYS.TASK_AUTO_FLUSH_MODE,
   "off",
 );
