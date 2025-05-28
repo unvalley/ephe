f006841122e660e73a530a1460b2314357b1fc24
unvalley <38400669+unvalley@users.noreply.github.com>
2025-05-28 22:01:40 +0900

    Revert "feat: revive command + k (#28)" (#30)
    This reverts commit 341254e8051e650e67220e2fef61f2cabc31b304.

--- src/page/editor-page.tsx
+++ src/page/editor-page.tsx
@@ -1,6 +1,5 @@
 import "../globals.css";
 import { usePaperMode } from "../utils/hooks/use-paper-mode";
-import { useEditorWidth } from "../utils/hooks/use-editor-width";
 import { Footer, FooterButton } from "../utils/components/footer";
 import { CommandMenu } from "../features/menu/command-menu";
 import { CodeMirrorEditor } from "../features/editor/codemirror/codemirror-editor";
@@ -9,19 +8,16 @@ import { HoursDisplay } from "../features/time-display/hours-display";
 import { Link } from "react-router-dom";
 import { EPHE_VERSION } from "../utils/constants";
 import { useCommandK } from "../utils/hooks/use-command-k";
-import { useMarkdownEditor } from "../features/editor/codemirror/use-markdown-editor";
 
 export const EditorPage = () => {
-  const { paperModeClass, paperMode, cyclePaperMode } = usePaperMode();
-  const { editorWidth, toggleEditorWidth } = useEditorWidth();
+  const { paperModeClass } = usePaperMode();
   const { isCommandMenuOpen, toggleCommandMenu } = useCommandK();
-  const { editor, content, formatDocument, insertText } = useMarkdownEditor();
 
   return (
     <div className={`flex h-screen flex-col overflow-hidden antialiased ${paperModeClass}`}>
       <div className="relative flex flex-1 overflow-hidden">
         <div className="z-0 flex-1">
-          <CodeMirrorEditor editorRef={editor} />
+          <CodeMirrorEditor />
         </div>
       </div>
 
@@ -37,19 +33,7 @@ export const EditorPage = () => {
         }
       />
 
-      {isCommandMenuOpen && (
-        <CommandMenu
-          open={isCommandMenuOpen}
-          onClose={toggleCommandMenu}
-          paperMode={paperMode}
-          cyclePaperMode={cyclePaperMode}
-          editorWidth={editorWidth}
-          toggleEditorWidth={toggleEditorWidth}
-          content={content}
-          formatDocument={formatDocument}
-          insertText={insertText}
-        />
-      )}
+      {isCommandMenuOpen && <CommandMenu open={isCommandMenuOpen} onClose={toggleCommandMenu} />}
     </div>
   );
 };
