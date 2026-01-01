8a21e4a19a196fd8508df22ee697e9cdafaef185
unvalley <kirohi.code@gmail.com>
2026-01-02 00:20:21 +0900

    chore: fix

--- src/utils/hooks/use-paper-mode.ts
+++ src/utils/hooks/use-paper-mode.ts
@@ -10,13 +10,13 @@ const PAPER_MODE_CLASSES = {
   dots: "bg-dots-paper",
 } as const;
 
+const modes = ["normal", "graph", "dots"] as const;
 const paperModeAtom = atomWithStorage<PaperMode>(LOCAL_STORAGE_KEYS.PAPER_MODE, "normal");
 
 export const usePaperMode = () => {
   const [paperMode, setPaperMode] = useAtom(paperModeAtom);
 
   const cyclePaperMode = () => {
-    const modes = ["normal", "graph", "dots"] as const;
     const currentIndex = modes.indexOf(paperMode);
     const nextIndex = (currentIndex + 1) % modes.length;
     setPaperMode(modes[nextIndex]);
